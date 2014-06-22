a1bf6936fd81a833e6aef3ac34773c9665a054bf
John Bintz <john@coswellproductions.com>
2014-06-21 23:06:53 -0400

    Start reworking internals to support other Game Crafter part types.

---
+++ lib/svggvs/page/letter/small_shard.rb
@@ -0,0 +1,13 @@
+require 'svggvs/page/base'
+
+module SVGGVS
+  module Page
+    module Letter
+      class SmallShard < SVGGVS::Page::Base
+        CARDS_X = 11
+        CARDS_Y = 14
+      end
+    end
+  end
+end
+
